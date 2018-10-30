e4a3314b611062790652a7f416d54a5df5a20718
Matthew Molnar <matthew.molnar@gmail.com>
2018-10-29 21:36:49 -0400

    Draw a line from left to right

---
+++ fun.js
@@ -0,0 +1,49 @@
+"use strict";
+
+function init(){
+  const canvas = document.getElementById('screen');
+  const context = canvas.getContext('2d');
+  game(context);
+}
+
+function game(context){
+  let imageData = context.getImageData(0, 0, 640, 480);
+  let data = imageData.data;
+  let buffer = data.buffer;
+  let data8 = new Uint8Array(buffer); //Per-color access
+
+  let newDataBuffer = new ArrayBuffer(data.length);
+  let newData8 = new Uint8ClampedArray(newDataBuffer);
+
+  setInterval(()=>{
+    gameLoop(newData8);
+    data.set(newData8);
+    context.putImageData(imageData, 0, 0);
+  }, 10)
+}
+
+const state = {line: 0};
+function gameLoop(src8){
+  // alter line data
+  const line = state.line;
+  state.line = line > 640 ? 0 : line + 1;
+
+  for (let i = 0; i < src8.length; i+=4){
+    // draw background
+    src8[i] = 0xFF;
+    src8[i+1] = 0;
+    src8[i+2] = 0;
+    src8[i+3] = 0xFF;
+
+    // draw vertical line
+    if ((i/4 % 640) === line){
+      src8[i] = 0;
+      src8[i+1] = 0;
+      src8[i+2] = 0;
+      src8[i+3] = 0xFF;
+    }
+  }
+
+}
+
+init();
