b01f45d334ad970bd05c09650c8512ec7a3c752f
Matthew Molnar <matthew.molnar@gmail.com>
2018-10-29 22:17:59 -0400

    Reorganize

--- fun.js
+++ fun.js
@@ -15,44 +15,58 @@ async function game(context){
   let newDataBuffer = new ArrayBuffer(data.length);
   let newData8 = new Uint8ClampedArray(newDataBuffer);
 
+  // game tick
   setInterval(()=>{
     gameLoop(newData8);
     data.set(newData8);
   }, 10);
 
-  async function* run(path) {
-    while (true){
-      yield await (new Promise(r => window.requestAnimationFrame(r)));
-    }
-  }
-
-  for await (const frame of run()){
+  // render tick
+  for await (const frame of getFrame()) {
     context.putImageData(imageData, 0, 0);
   }
 }
 
-const state = {line: 0};
 function gameLoop(src8){
-  // alter line data
+  // move line
   const line = state.line;
   state.line = line > 640 ? 0 : line + 1;
 
   for (let i = 0; i < src8.length; i+=4){
-    // draw background
-    src8[i] = 0xFF;
+    drawBackground(i, src8);
+    drawVerticalLine(i, line, src8);
+  }
+}
+
+function drawBackground(i, src8){
+  src8[i] = 0xFF;
+  src8[i+1] = 0;
+  src8[i+2] = 0;
+  src8[i+3] = 0xFF;
+}
+
+function drawVerticalLine(i, line, src8){
+  if ((i/4 % 640) === line){
+    src8[i] = 0;
     src8[i+1] = 0;
     src8[i+2] = 0;
     src8[i+3] = 0xFF;
-
-    // draw vertical line
-    if ((i/4 % 640) === line){
-      src8[i] = 0;
-      src8[i+1] = 0;
-      src8[i+2] = 0;
-      src8[i+3] = 0xFF;
-    }
   }
+}
 
+const state = {line: 0, menu: true};
+
+const menu = {
+  options: [
+    {id: 1, name:"New Game"},
+    {id: 1, name:"Quit"}
+  ]
+};
+
+async function* getFrame(path) {
+  while (true){
+    yield await (new Promise(r => window.requestAnimationFrame(r)));
+  }
 }
 
 init();
